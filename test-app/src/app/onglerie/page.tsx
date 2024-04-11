e0b4c431bacb4a2c76ca1d651258abfd9dceb340
Alvina LE SAINT <ale-sain@paul-f4Br4s7.clusters.42paris.fr>
2024-04-11 19:54:33 +0200

    same

--- test-app/src/app/onglerie/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function OngleriePage() {
-  return (
-    <div>OngleriePage</div>
-  )
-}
--- test-app/src/app/reflexologie/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ReflexologyPage() {
-  return (
-    <div>ReflexologyPage</div>
-  )
-}
