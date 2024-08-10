987198b7b92d77d0ee714e862d743a91737e171e
alvinouille <enjoy011113@gmail.com>
2024-08-10 21:52:46 +0200

    modification of footer

--- test-app/src/app/footer.tsx
+++ test-app/src/app/footer.tsx
@@ -10,21 +10,20 @@ export default function Footer() {
                 <div className="content-footer">
                     <h2  className="h2-footer">Institut Secrets de Beauté</h2>
                     <div className="wrapper-footer">
-                        <p className="p-footer">INSTITUT CLIMATISE</p>
-                        <p className="p-footer">Adresse : 10 rue du Distro, 56690 LANDEVANT</p>
+                        <p className="p-footer">INSTITUT CLIMATISÉ</p>
+                        <p className="p-footer">Adresse : 10 rue du Distro, 56690 LANDÉVANT</p>
                     </div>
                     <div className="wrapper-footer">
                         <p className="p-footer">Tel : 06 12 35 08 77</p>
                         <p className="p-footer">Rendez-vous par SMS</p>
                     </div>
                     <div className="wrapper-footer">
-                        <p className="p-footer">Email : sophie.lesaint56@gmail.com</p>
+                        <p className="p-footer">Pas de règlement en CB</p>
                     </div>
                     <div className="wrapper-footer">
                         <p className="p-footer">Horaires d&apos;ouverture :</p>
                         <p className="p-footer heavy">Ouvert sur rendez-vous</p>
                         <p className="p-footer">du Lundi au Vendredi : de 9h00 a 19h00</p>
-                        <p className="p-footer">le Samedi : de 9h00 a 13h00</p>
                         <p className="p-footer">Fermé le mercredi</p>
                     </div>
                 </div>
